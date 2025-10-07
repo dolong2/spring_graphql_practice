63a3c99f702d5cb36013630ff56dc2ef0d078213
dolong2 <k01066624566@gmail.com>
2025-10-07 21:26:19 +0900

    [chore] 에러코드 네이밍 수정

--- src/main/kotlin/com/practice/graphql/global/exception/collections/MemberNotExistException.kt
+++ src/main/kotlin/com/practice/graphql/global/exception/collections/MemberNotExistException.kt
@@ -3,5 +3,5 @@ package com.practice.graphql.global.exception.collections
 import com.practice.graphql.global.exception.ErrorCode
 
 class MemberNotExistException: BasicException(
-    ErrorCode.NOT_EXIST_MEMBER
+    ErrorCode.MEMBER_NOT_EXIST
 )
--- src/main/kotlin/com/practice/graphql/global/exception/collections/MemberNotSameException.kt
+++ src/main/kotlin/com/practice/graphql/global/exception/collections/MemberNotSameException.kt
@@ -3,5 +3,5 @@ package com.practice.graphql.global.exception.collections
 import com.practice.graphql.global.exception.ErrorCode
 
 class MemberNotSameException: BasicException(
-    ErrorCode.NOT_SAME_WRITER
+    ErrorCode.WRITER_NOT_SAME
 )
