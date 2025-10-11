277885c7ef3a2c4905f1db26d51445b5fe00d624
dolong2 <k01066624566@gmail.com>
2025-10-11 23:09:17 +0900

    [del] PostingUpdateReq에서 toEntity 메서드 제거

--- src/main/kotlin/com/practice/graphql/domain/posting/presentation/dto/request/PostingUpdateReq.kt
+++ src/main/kotlin/com/practice/graphql/domain/posting/presentation/dto/request/PostingUpdateReq.kt
@@ -1,16 +1,6 @@
 package com.practice.graphql.domain.posting.presentation.dto.request
 
-import com.practice.graphql.domain.member.Member
-import com.practice.graphql.domain.posting.Posting
-
 class PostingUpdateReq(
     val title:String,
     val content: String,
-){
-    fun toEntity(member: Member) =
-        Posting(
-            title = title,
-            content = content,
-            writer = member,
-        )
-}
+)
