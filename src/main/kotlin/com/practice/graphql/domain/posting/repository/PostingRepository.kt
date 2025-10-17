cad18f7489cbf7b1dcd1af1fe8745bf099a9ffb4
dolong2 <k01066624566@gmail.com>
2025-10-17 22:39:37 +0900

    [feat] PostingFacade에 topic을 통한 조회 메서드 추가

--- src/main/kotlin/com/practice/graphql/domain/posting/repository/PostingRepository.kt
+++ src/main/kotlin/com/practice/graphql/domain/posting/repository/PostingRepository.kt
@@ -1,9 +1,11 @@
 package com.practice.graphql.domain.posting.repository
 
 import com.practice.graphql.domain.posting.Posting
+import com.practice.graphql.domain.topic.Topic
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.graphql.data.GraphQlRepository
 
 @GraphQlRepository
 interface PostingRepository : JpaRepository<Posting, Long>{
+    fun findAllByTopic(topic: Topic): List<Posting>
 }
