630c22ec74245753b46a64285ee8a3de1b40ec0d
dolong2 <k01066624566@gmail.com>
2025-10-23 23:04:27 +0900

    [feat] comment 엔티티 추가

---
+++ src/main/kotlin/com/practice/graphql/domain/comment/Comment.kt
@@ -0,0 +1,22 @@
+package com.practice.graphql.domain.comment
+
+import com.practice.graphql.domain.posting.Posting
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
+
+@Entity
+class Comment(
+    @Id
+    @GeneratedValue
+    val id: Long = 0L,
+    var content: String,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    var updatedAt: LocalDateTime? = null,
+    @ManyToOne
+    @JoinColumn(name = "posting_id")
+    val posting: Posting
+)
