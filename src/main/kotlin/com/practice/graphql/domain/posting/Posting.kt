1f5f10fc2d35858d184f949d079fa769b55dbd83
dolong2 <k01066624566@gmail.com>
2025-10-23 23:05:51 +0900

    [feat] posting 엔티티에 comment 연관관계 추가

--- src/main/kotlin/com/practice/graphql/domain/posting/Posting.kt
+++ src/main/kotlin/com/practice/graphql/domain/posting/Posting.kt
@@ -1,5 +1,6 @@
 package com.practice.graphql.domain.posting
 
+import com.practice.graphql.domain.comment.Comment
 import com.practice.graphql.domain.member.Member
 import com.practice.graphql.domain.posting.presentation.dto.request.PostingUpdateReq
 import com.practice.graphql.domain.topic.Topic
@@ -8,6 +9,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 
 @Entity
 class Posting(
@@ -19,6 +21,8 @@ class Posting(
     @ManyToOne
     @JoinColumn(name = "topic_id")
     val topic: Topic,
+    @OneToMany
+    val comments: MutableList<Comment> = mutableListOf(),
 ){
     @Id
     @GeneratedValue
