67bf708b508b90a7697dae5f0c5698f53e800092
dolong2 <k01066624566@gmail.com>
2025-10-25 00:04:50 +0900

    [feat] 코멘트 작성 스키마 추가

--- src/main/kotlin/com/practice/graphql/domain/comment/presentation/CommentController.kt
+++ src/main/kotlin/com/practice/graphql/domain/comment/presentation/CommentController.kt
@@ -2,6 +2,7 @@ package com.practice.graphql.domain.comment.presentation
 
 import com.practice.graphql.domain.comment.presentation.dto.request.WriteCommentReqDto
 import com.practice.graphql.domain.comment.service.WriteCommentService
+import com.practice.graphql.global.response.SuccessResponse
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.stereotype.Controller
@@ -16,4 +17,5 @@ class CommentController(
         @Argument writeCommentReqDto: WriteCommentReqDto
     ) =
         writeCommentService.execute(postingId, writeCommentReqDto)
+            .let { SuccessResponse }
 }
